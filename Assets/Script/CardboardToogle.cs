6bf9b868a99d94d12a7dc4196582c4b484885cb6
Updesh Pr. Singh <updesh071@gmail.com>
2022-10-02 11:48:41 +0530

    hlo

--- Assets/Script/CardboardToogle.cs
+++ Assets/Script/CardboardToogle.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class CardboardToogle : MonoBehaviour
+using UnityEngine.VR;
+public class CardBoardToggle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int vrModeInt;
+    public void ToggleVRMode()
     {
-        
+        if (PlayerPrefs.GetInt("Vr Mode") == 1)
+        {
+            vrModeInt = 0;
+        }
+        if (PlayerPrefs.GetInt("Vr Mode") == 0)
+        {
+            vrModeInt = 1;
+        }
+        PlayerPrefs.SetInt("Vr Mode", vrModeInt);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerPrefs.GetInt("Vr Mode") == 1)
+        {
+            CardBoard.SDK.VRModeEnabled = true;
+        }
+        if (PlayerPrefs.GetInt("Vr Mode") == 0)
+        {
+            CardBoard.SDK.VRModeEnabled = false;
+        }
     }
 }
