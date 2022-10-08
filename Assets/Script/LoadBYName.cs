c50cebf4d8dec78b7715e64afa40e87e82a7fab5
Updesh Pr. Singh <updesh071@gmail.com>
2022-10-08 13:41:35 +0530

    update bro

---
+++ Assets/Script/LoadBYName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+public class LoadBYName : MonoBehaviour
+{
+
+    public void VerifyDeviceName(string newDeviceName)
+    {
+        if (newDeviceName == "none")
+        {
+            Debug.Log("Will load none device");
+            StartCoroutine(LoadDevice(newDeviceName));
+
+        }
+        if (newDeviceName == "cardboard")
+        {
+            Debug.Log("Will load cardboard device");
+            StartCoroutine(LoadDevice(newDeviceName));
+        }
+        else
+        {
+            Debug.Log("Can't find device with name " + newDeviceName);
+        }
+
+    }
+
+    IEnumerator LoadDevice(string newDeviceName)
+    {
+        XRSettings.LoadDeviceByName(newDeviceName);
+        yield return new WaitForSeconds(10);
+        XRSettings.enabled = true;
+        Debug.Log("Loaded " + newDeviceName);
+    }
+}
---
+++ Assets/Script/VRmodeon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class VRmodeon : MonoBehaviour
+{
+    public void mode()
+    {
+        XRSettings.LoadDeviceByName("Cardboad");
+        XRSettings.enabled = true;
+    }
+   
+}
