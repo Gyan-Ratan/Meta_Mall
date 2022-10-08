f1543a9b866ec796b9356b1c155e66d677b49a2a
Updesh Pr. Singh <updesh071@gmail.com>
2022-10-08 17:00:38 +0530

    up

--- Assets/Script/TestToogleVR.cs
+++ Assets/Script/TestToogleVR.cs
@@ -7,27 +7,48 @@ public class TestToogleVR : MonoBehaviour
 {
     public void Awake()
     {
-        SwitchToVR();
+        SwitchTo2D();
     }
-    // Call via `StartCoroutine(SwitchToVR())` from your code. Or, use
-    // `yield SwitchToVR()` if calling from inside another coroutine.
-    IEnumerator SwitchToVR()
+    // Call via `StartCoroutine(SwitchTo2D())` from your code. Or, use
+    // `yield SwitchTo2D()` if calling from inside another coroutine.
+    IEnumerator SwitchTo2D()
     {
-        // Device names are lowercase, as returned by `XRSettings.supportedDevices`.
-        string desiredDevice = "daydream"; // Or "cardboard".
+        // Empty string loads the "None" device.
+        XRSettings.LoadDeviceByName("");
 
-        // Some VR Devices do not support reloading when already active, see
-        // https://docs.unity3d.com/ScriptReference/XR.XRSettings.LoadDeviceByName.html
-        if (String.Compare(XRSettings.loadedDeviceName, desiredDevice, true) != 0)
-        {
-            XRSettings.LoadDeviceByName(desiredDevice);
+        // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
+        yield return null;
 
-            // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
-            yield return null;
-        }
+        // Not needed, since loading the None (`""`) device takes care of this.
+        // XRSettings.enabled = false;
 
-        // Now it's ok to enable VR mode.
-        XRSettings.enabled = true;
+        // Restore 2D camera settings.
+        ResetCameras();
     }
 
+    // Resets camera transform and settings on all enabled eye cameras.
+    void ResetCameras()
+    {
+        // Camera looping logic copied from GvrEditorEmulator.cs
+        for (int i = 0; i < Camera.allCameras.Length; i++)
+        {
+            Camera cam = Camera.allCameras[i];
+            if (cam.enabled && cam.stereoTargetEye != StereoTargetEyeMask.None)
+            {
+
+                // Reset local position.
+                // Only required if you change the camera's local position while in 2D mode.
+                cam.transform.localPosition = Vector3.zero;
+
+                // Reset local rotation.
+                // Only required if you change the camera's local rotation while in 2D mode.
+                cam.transform.localRotation = Quaternion.identity;
+
+                // No longer needed, see issue github.com/googlevr/gvr-unity-sdk/issues/628.
+                // cam.ResetAspect();
+
+                // No need to reset `fieldOfView`, since it's reset automatically.
+            }
+        }
+    }
 }
